21c36387fd1c045ad9eead18400e949e87594ae8
heba.fahmy <heba.fahmy@cyshield.com>
2025-09-16 15:52:32 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import nextra from 'nextra';
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+});
+
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // redirect to /home instead of page.tsx (if i want)
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/resources',
+        permanent: true,
+      },
+    ];
+  },
+});
---
+++ src/app/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  resources: 'Introduction',
+  'getting-started': 'Getting Started',
+  confg: 'Configuration',
+  components: 'Built-In Components',
+};
